e21bd65ae8bdc1a062792b22d4d130697dd238ef
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-04-09 16:57:23 +0100

    changes made to screen3

--- javascript/game-won.js
+++ javascript/game-won.js
@@ -6,6 +6,7 @@ function gameWon(){
 
     wonGameText.style.color = "white";
     wonGameText.style.fontSize = "24px";
+    wonGameText.style.padding = "2%";
     document.body.style.backgroundColor = "black";
     
     //the following is to stay commented until I find a way to access the play video button
@@ -16,7 +17,7 @@ function gameWon(){
     document.body.style.backgroundColor = "whitesmoke";
     */
     
-    video.style = "position: absolute; top: 47%; left: 50%; transform: translate(-50%, -50%);";
+    video.style = "position: absolute; top: 45%; left: 50%; transform: translate(-50%, -50%);";
     slayAgaintButton.style = "position: absolute; top: 85%;";
     woundsButton.style = "position: absolute; top: 80%;";
     slayAgaintButton.addEventListener('click', startGame );
