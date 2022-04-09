96a12b9925744a7d11b071e312c75173f10fabc0
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-04-09 23:04:47 +0100

    minor aesthetic changes

--- javascript/screen2-lost.js
+++ javascript/screen2-lost.js
@@ -1,15 +1,19 @@
 function gameLost(){
+
     gameBoardElement.classList.add("hidden");
     gameLostElement.classList.remove("hidden");
+    
     reset();
+
     attackMusic.pause();
     playLostScreenMusic();
-    
+
     document.body.style.backgroundColor = "black";
     footerElement.style.backgroundColor = "whitesmoke";
+
     lostGameText.style.padding = "10%";
     lostGameText.style.fontSize = "48px";
-    tryAgainButton.addEventListener('click', startGame );
 
+    tryAgainButton.addEventListener('click', startGame );
     regroupButton.addEventListener('click', backToStart );
 }
--- javascript/screen3-won.js
+++ javascript/screen3-won.js
@@ -1,15 +1,18 @@
 function gameWon(){
-    pauseMusic();
-    wowMusic.play();
     gameBoardElement.classList.add("hidden");
     gameWonElement.classList.remove("hidden");
+    
     reset();
+
+    pauseMusic();
+    wowMusic.play();
+
+    document.body.style.backgroundColor = "black";
+    footerElement.style.backgroundColor = "whitesmoke";
 
     wonGameText.style.color = "white";
     wonGameText.style.fontSize = "24px";
     wonGameText.style.padding = "2%";
-    document.body.style.backgroundColor = "black";
-    footerElement.style.backgroundColor = "whitesmoke";
     
     //the following is to stay commented until I find a way to access the play video button
     /*music = new Audio('./assets/music/GameOver.mp3');
@@ -20,9 +23,10 @@ function gameWon(){
     */
     
     video.style = "position: absolute; top: 45%; left: 50%; transform: translate(-50%, -50%);";
+    
     slayAgaintButton.style = "position: absolute; top: 85%;";
     woundsButton.style = "position: absolute; top: 80%;";
-    slayAgaintButton.addEventListener('click', startGame );
     
+    slayAgaintButton.addEventListener('click', startGame );
     woundsButton.addEventListener('click', backToStart );
 }
